d98ef581ed06437a2dfbf0d3d68555d0346b8c68
sunzhuoyi <zhuoyi.sun@ele.me>
2017-02-08 16:22:57 +0800

    引入模板引擎Nunjucks

--- app/app.js
+++ app/app.js
@@ -1,8 +1,25 @@
 import koa from 'koa';
+import bodyParder from 'koa-bodyparser';
+import session from 'koa-session';
+import staticServer from 'koa-static';
+
 const app = koa();
 
-app.use(function *(){
+//一个简单的koa服务器
+
+/*app.use(function *(){
 	this.body = 'Hello Koa';
 });
 
+app.listen(8080);*/
+
+//koa-static 用于创建静态文件服务
+app.use(staticServer(__dirname+'/public'));
+//session
+app.keys = ['some secret hurr'];
+app.use(session(app));
+//body parser
+app.ues(function *() {
+	this.body = 'Hello Koa';
+});
 app.listen(8080);
